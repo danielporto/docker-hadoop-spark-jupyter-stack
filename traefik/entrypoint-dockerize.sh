ce185a72ae76f1d8dcc7338b42d66c9556628f1c
Daniel Porto <daniel.porto@gmail.com>
2022-10-03 11:05:38 +0100

    reenable https for traefik local accesses and fix endpoints for hadoop services

--- traefik/entrypoint-dockerize.sh
+++ traefik/entrypoint-dockerize.sh
@@ -5,8 +5,12 @@ echo "========================================="
 echo "Parsing templates"
 echo "========================================="
 dockerize -template /etc/traefik/traefik.tmpl:/etc/traefik/traefik.yml
+echo "========================================="
+cat /etc/traefik/traefik.yml
+
+echo "========================================="
 dockerize -template /etc/traefik/dynamic.tmpl:/etc/traefik/dynamic.yml
+cat /etc/traefik/dynamic.yml
 echo "templates parsed"
 echo "continue with traefik entrypoint original workflow."
-cat /etc/traefik/traefik.yml
 traefik "$@"
